08ad2d014f77007364fd68788cd550d92f0db8a2
aebaack <aebaack@gmail.com>
2019-02-01 11:47:32 -0700

    Count number of phone numbers in file

--- lab2_scripts.sh
+++ lab2_scripts.sh
@@ -13,4 +13,6 @@ read file
 #Test regular expression against the file
 grep $regExp $file
 
-
+#Find all phone numbers
+echo "Total number of phone numbers: "
+grep -c "[1-9][0-9]\{2\}-[0-9]\{3\}-[0-9]\{4\}" "regex_practice.txt"
